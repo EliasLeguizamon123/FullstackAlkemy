49982c938e8d8d12a9231b792c110ac7bc944285
Elías Leguizamón <elias.leguizamon1997@gmail.com>
2021-08-24 19:36:54 -0300

    allow CORS for all acess

--- src/app.js
+++ src/app.js
@@ -10,7 +10,13 @@ const app = express();
 //Settings
 app.set('port', process.env.PORT || 8080);
 app.set('views', path.resolve(__dirname, 'views'));
-app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+     next();
+});
 
 app.listen(process.env.PORT, ()=>{
     console.log(`App listen on port ${process.env.PORT}`);
